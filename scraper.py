0f221c5fc0c41099f66311c218ae5fb4b88e258c
Sophie Chou <sbchou@gmail.com>
2013-03-25 13:54:05 -0400

    scraper scrapes date, title, description from bwog free food homepage

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from urllib2 import *
 from bs4 import BeautifulSoup as BS
 
 """
-for name:
+for title:
     <div class = "post-title"><a href = "URL">STUFF WE WANT</a></div>
 
 for decscription:
@@ -22,7 +22,7 @@ def get_page(url):
         page_html = urlopen(url)
         return page_html
     except URLError as e:
-        print "MEW"
+        print "INVALID URL"
         return None 
 
 def get_titles(content):
@@ -32,20 +32,36 @@ def get_titles(content):
     
     #for each <div class="post-title"> get text </div>
     for div in soup.findAll("div", {"class":"post-title"}):
-        titles.append(div.get_text())
+        titles.append(div.get_text().encode('utf-8').strip())
 
     return titles
 
+def get_descriptions(content):
+    "get description/post-content of an event"
+    soup = BS(content)
+    descriptions = []
+    for div in soup.findAll("div", {"class":"post-content"}):
+        descriptions.append(div.get_text().encode('utf-8').strip())
+    return descriptions
+
+def get_dates(content):
+    "get date of post"
+    soup = BS(content)
+    dates =[]
+    for div in soup.findAll("div", {"class":"post-datetime"}):
+        dates.append(div.get_text().encode('utf-8').strip())
+    return dates
 
 #scrape the most current free food page
-
-bwog_url = "http://bwog.com/tag/free-food/" 
+bwog_url = "http://bwog.com/tag/free-food/"
 
 titles = get_titles(get_page(bwog_url))
-for title in titles:
-    print title
+descriptions = get_descriptions(get_page(bwog_url))
+dates = get_dates(get_page(bwog_url))
 
-
+for title, description, date in zip(titles, descriptions, dates):
+    print date, title, description
+    print ""
 
 
 
